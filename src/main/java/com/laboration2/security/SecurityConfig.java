75dfdd5d0c80f25690d239e0f87e62c14d9b68d2
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-12-06 09:28:03 +0100

    Add dev security

--- src/main/java/com/laboration2/security/SecurityConfig.java
+++ src/main/java/com/laboration2/security/SecurityConfig.java
@@ -4,22 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.
-                httpBasic(Customizer.withDefaults())
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/categories").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/api/categories").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/api/categories").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categories/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/locations").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/locations").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/locations").permitAll()
                         .requestMatchers(HttpMethod.PATCH, "/api/locations").hasRole("USER")
                         .requestMatchers(HttpMethod.DELETE, "/api/locations").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/api/locations/*").permitAll()
@@ -28,5 +32,4 @@ public class SecurityConfig {
                 )
                 .build();
     }
-
 }
