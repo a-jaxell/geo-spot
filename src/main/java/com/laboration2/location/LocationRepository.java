031700771f1aa279282a5cc69064875e8d284ddb
a-jaxell <alvar.jaxell@gmail.com>
2024-05-09 15:20:50 +0200

    Add Location fetch based on user auth

--- src/main/java/com/laboration2/location/LocationRepository.java
+++ src/main/java/com/laboration2/location/LocationRepository.java
@@ -1,10 +1,10 @@
 package com.laboration2.location;
 
-import com.laboration2.location.Location;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -13,6 +13,10 @@ public interface LocationRepository extends ListCrudRepository<Location, Integer
 
     @Query("SELECT l FROM Location l WHERE ST_Within(l.coordinates, ST_GeomFromText(:polygon, 4326)) = true")
     List<Location> findWithinPolygon(String polygon);
+
+    List<Location> findByVisibleTrue();
+
+    Collection<? extends Location> findByVisibleFalseAndUserFirstNameEquals(String username);
 }
 // List<Place> findPlacesByUserId(string userId);
 //Entitygraph använder vi om vi har fetchType LAZY på en @ManyToOne relation
--- src/main/java/com/laboration2/location/LocationService.java
+++ src/main/java/com/laboration2/location/LocationService.java
@@ -1,6 +1,9 @@
 package com.laboration2.location;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +21,14 @@ public class LocationService {
     }
 
     public List<Location> getAllLocations() {
-        return locationRepository.findAll();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = null;
+        List<Location> locations = locationRepository.findByVisibleTrue();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            username = userDetails.getUsername();
+            locations.addAll(locationRepository.findByVisibleFalseAndUserFirstNameEquals(username));
+        }
+        return locations;
     }
 
     public Location getLocationById(int id) {
